47a29da5d7869561504509bd45d9a8590c89f516
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-08 17:21:53 +0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-hostnamectl set-hostname ISP; exec bash
-mkdir /etc/sysconfig/network-scripts/ens19 /etc/sysconfig/network-scripts/ens20 /etc/sysconfig/network-scripts/ens21
-echo "DEVICE=ens19\nBOOTPROTO=static\nONBOOT=yes\nIPADDR=1.1.1.1\nNETMASK=255.255.255.252" > /etc/sysconfig/network-scripts/ifcfg-ens19
-echo "DEVICE=ens20\nBOOTPROTO=static\nONBOOT=yes\nIPADDR=3.3.3.1\nNETMASK=255.255.255.0" > /etc/sysconfig/network-scripts/ifcfg-ens20
-echo "DEVICE=ens21\nBOOTPROTO=static\nONBOOT=yes\nIPADDR=2.2.2.1\nNETMASK=255.255.255.252" > /etc/sysconfig/network-scripts/ifcfg-ens21
+nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses 2024:1::1/64
+nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 1.1.1.1/30
+
+
 
