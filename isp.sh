63983928722b6d6f53a2a0ad860d9d39101c1671
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-11 09:56:16 +0300

    Add files via upload

--- isp.sh
+++ isp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+dnf remove -y git
 nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses 2024:1::1/64
 nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 1.1.1.1/30
 
