8fbe86f8761edcb04ec79223cf67778eac501f7d
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-11 08:38:49 +0300

    Add files via upload

--- isp.sh
+++ isp.sh
@@ -9,7 +9,7 @@ nmcli con modify Проводное\ подключение\ 3 ipv6.method manua
 nmcli con modify Проводное\ подключение\ 3 ipv4.method manual ipv4.addresses 2.2.2.1/30
 
 echo -e "net.ipv4.ip_forward=1\nnet.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-sysctl-p
+sysctl -p
 dnf install -y nftables
 echo -e 'table inet my_nat {\n\tchain my_masquerade {\n\ttype nat hook postrouting priority srcnat;\n\toifname "ens18" masquerade\n\t}\n}' > /etc/nftables/isp.nft
 echo 'include "/etc/nftables/isp.nft"' >> /etc/sysconfig/nftables.conf
